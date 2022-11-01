b61143a2e41f072a7a6e6eb65fc796a7c5594c2c
Marek Truska <truskamarek@centrum.sk>
2022-11-01 20:25:31 +0100

    Added dynamic change of grid based on the user input via slider bar

--- main.js
+++ main.js
@@ -1,45 +1,42 @@
-function createGrid() {
-  //Access div containing the grid
-  const container = document.getElementById("container");
+const container = document.getElementById("container");
+let slider = document.getElementById('myRange');
+let reset = document.querySelector('.reset');
 
-  //Variables that holds grid rows and columns
-  let rows = 16;
-  let columns = 16;
+slider.addEventListener("change", () => {
 
+  let rows = slider.value;
+
+  container.innerHTML ="";
+  
   //Variables that governs squares width and height based on how many rows 
   //and columns there are
-  let squareWidth = container.clientWidth/columns-2;
-  let squareHeight = container.clientHeight/rows-2;
-
+  let squareWidth = container.clientWidth/rows;
+  let squareHeight = container.clientHeight/rows;
+  
   //Loop that creates the desired grid
-  for (let j = 0; j < rows*columns; j++) {
+  for (let j = 0; j < rows*rows; j++) {
     const newDiv = document.createElement("div");
     newDiv.style.height = `${squareHeight}px`;
     newDiv.style.width = `${squareWidth}px`;
     newDiv.classList.add('square');
     container.appendChild(newDiv);
   }
-}
 
-container.onload = createGrid();
+  let squares = document.querySelectorAll('.square');
 
-let squares = document.querySelectorAll('.square');
-
-squares.forEach(square => {
-  square.addEventListener("mouseover", () => {
-    square.style.background = "black";
-    square.style.transition = "background 0.5s ease-in-out";
-  });
-});
-
-let reset = document.querySelector('.reset');
-
-reset.addEventListener("click", () => {
   squares.forEach(square => {
-    square.style.background = "white";
+    square.addEventListener("mouseover", () => {
+      square.classList.add('hover-square');
+    });
   });
-});
 
+  reset.addEventListener("click", () => {
+    squares.forEach(square => {
+      square.classList.remove('hover-square');
+    });
+  });
+
+});
 
 
 
