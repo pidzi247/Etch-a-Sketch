4ce6da51d556a95250b10d5ffcee76e24ff47929
Marek Truska <truskamarek@centrum.sk>
2022-11-06 23:06:56 +0100

    Added buttons for default black color, random color, eraser and clear button

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
+//Variables to initiate default size of grid and color of grid squares
 const defaultSize = 16;
+const defaultColor = 'black';
 
+let activeColor = defaultColor;
+let gridSize = defaultSize;
+
+
+//Variables that hold access to DOM elements
 const container = document.getElementById("container");
 const black = document.getElementById('black');
-const randomColor = document.getElementById('random');
+const random = document.getElementById('random');
+const reset = document.getElementById('reset');
+const eraser = document.getElementById('eraser');
+
 let slider = document.getElementById('myRange');
-let reset = document.querySelector('.reset');
 
 
+//Function that creates default basic grid on start up
 function createGrid(size) {
 
   //Variables that governs squares width and height based on how many rows 
@@ -25,14 +35,55 @@ function createGrid(size) {
 }
 
 function color(event) {
-  event.target.style.background = "black";
+  if(activeColor === 'black') {
+    event.target.style.background = "black";
+  } else if(activeColor === 'random') {
+    event.target.style.background = randomColor();
+  } else if(activeColor === 'white'){
+    event.target.style.background = 'white';
+  }
 }
 
-black.onclick = () => { black.classList.add('active')};
+function activateButton(color) {
+  switch(color) {
+    case 'black':
+      activeColor = 'black';
+      random.classList.remove('active');
+      eraser.classList.remove('active');
+      black.classList.add('active');
+      break;
+    case 'green':
+      activeColor = 'random';
+      black.classList.remove('active');
+      eraser.classList.remove('active');
+      random.classList.add('active');
+      break;
+    case 'white':
+      activeColor = 'white';
+      random.classList.remove('active');
+      black.classList.remove('active');
+      eraser.classList.add('active');
+      break;
+  }
+}
+
+function randomColor() {
+  let red = Math.floor(Math.random() * 255);
+  let green = Math.floor(Math.random() * 255);
+  let blue = Math.floor(Math.random() * 255);
+  let rgbColor = `rgb(${red}, ${green}, ${blue})`;
+  return rgbColor;
+}
+
+
+black.onclick = () => activateButton('black');
+random.onclick = () => activateButton('green');
+eraser.onclick = () => activateButton('white');
+reset.onclick = () => { container.innerHTML = ''; createGrid(gridSize)};
+
 
 window.onload = createGrid(defaultSize);
 
-console.log(black);
 
 
 
