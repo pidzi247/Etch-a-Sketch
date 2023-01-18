95fcb309cc5d55123cf28d3123aee32082ef65d4
Marek Truska <truskamarek@centrum.sk>
2023-01-18 10:26:47 +0100

    Refactoring and reformatting

--- script.js
+++ script.js
@@ -1,45 +1,34 @@
-//Variables to initiate default size of grid and color of grid squares
 const defaultSize = 16;
-const defaultColor = '#000000';
-const defaultButton = 'black';
+const defaultColor = "#000000";
+const defaultButton = "black";
 
 let activeColor = defaultColor;
 let activeButton = defaultButton;
 let gridSize = defaultSize;
-
-
-//Variables that hold access to DOM elements
-const container = document.querySelector(".sketch-area");
-const colorPalette = document.getElementById('color-pallete');
-const random = document.getElementById('random');
-const reset = document.getElementById('reset');
-const eraser = document.getElementById('eraser');
-const colorWheel = document.getElementById('color-wheel');
-let slider = document.getElementById('myRange');
-let gridValue = document.getElementById('grid-size');
-
 let toggle = false;
-document.body.addEventListener('mousedown', () => toggle = true)
-document.body.addEventListener('mouseup', () => toggle = false)
 
+const container = document.getElementById("sketchArea");
+const colorPalette = document.getElementById("colorPallete");
+const colorWheel = document.getElementById("colorWheel");
+const random = document.getElementById("random");
+const reset = document.getElementById("reset");
+const eraser = document.getElementById("eraser");
+const slider = document.getElementById("myRange");
+const gridValue = document.getElementById("gridSize");
 
-
-//Function that creates default basic grid on start up
+//Function that creates default grid on start up
 function createGrid(size) {
-
-  //Variables that governs squares width and height based on how many rows 
-  //and columns there are
+  //Variables that governs how many rows and columns there are
   container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
   container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-  
-  //Loop that creates the desired grid
-  for (let i = 0; i < size*size; i++) {
+
+  //Loop to create grid
+  for (let i = 0; i < size * size; i++) {
     const newDiv = document.createElement("div");
-    newDiv.classList.add('square');
-    newDiv.addEventListener('mouseover', color);
+    newDiv.classList.add("square");
+    newDiv.addEventListener("mouseover", color);
     container.appendChild(newDiv);
   }
-
 }
 
 function setColor(newColor) {
@@ -52,37 +41,37 @@ function changeButton(newButton) {
 }
 
 function color(e) {
-  if(!toggle) return;
-  if(activeButton === 'colorWheel') {
+  if (!toggle) return;
+  if (activeButton === "colorWheel") {
     e.target.style.background = activeColor;
-  } else if(activeButton === 'random') {
+  } else if (activeButton === "random") {
     e.target.style.background = randomColor();
-  } else if(activeButton === 'eraser'){
-    e.target.style.background = 'gray';
+  } else if (activeButton === "eraser") {
+    e.target.style.background = "gray";
   }
 }
 
 function activateButton(btn) {
-  switch(btn) {
-    case 'colorWheel':
-      random.classList.remove('active');
-      eraser.classList.remove('active');
-      colorPalette.classList.add('active');
+  switch (btn) {
+    case "colorWheel":
+      random.classList.remove("active");
+      eraser.classList.remove("active");
+      colorPalette.classList.add("active");
       break;
-    case 'random':
-      eraser.classList.remove('active');
-      colorPalette.classList.remove('active');
-      random.classList.add('active');
+    case "random":
+      eraser.classList.remove("active");
+      colorPalette.classList.remove("active");
+      random.classList.add("active");
       break;
-    case 'eraser':
-      random.classList.remove('active');
-      colorPalette.classList.remove('active');
-      eraser.classList.add('active');
+    case "eraser":
+      random.classList.remove("active");
+      colorPalette.classList.remove("active");
+      eraser.classList.add("active");
       break;
     default:
-      random.classList.remove('active');
-      colorPalette.classList.remove('active');
-      eraser.classList.remove('active');
+      random.classList.remove("active");
+      colorPalette.classList.remove("active");
+      eraser.classList.remove("active");
       break;
   }
 }
@@ -95,12 +84,25 @@ function randomColor() {
   return rgbColor;
 }
 
-slider.onchange = (e) => {gridSize = e.target.value; container.innerHTML = ""; createGrid(e.target.value);}
-colorWheel.oninput = (e) => setColor(e.target.value);
-colorPalette.onclick = () => changeButton('colorWheel');
-random.onclick = () => changeButton('random');
-eraser.onclick = () => changeButton('eraser');
-reset.onclick = () => { container.innerHTML = ''; changeButton('reset'); createGrid(gridSize)};
 
+//Event Listeners
+document.body.addEventListener("mousedown", () => (toggle = true));
+document.body.addEventListener("mouseup", () => (toggle = false));
+colorWheel.addEventListener("input", (e) => setColor(e.target.value));
+colorPalette.addEventListener("click", () => changeButton("colorWheel"));
+random.addEventListener("click", () => changeButton("random"));
+eraser.addEventListener("click", () => changeButton("eraser"));
+
+reset.addEventListener("click", () => {
+  container.innerHTML = "";
+  changeButton("reset");
+  createGrid(gridSize);
+});
+
+slider.addEventListener("change", (e) => {
+  gridSize = e.target.value;
+  container.innerHTML = "";
+  createGrid(e.target.value);
+});
 
 window.onload = createGrid(defaultSize);
